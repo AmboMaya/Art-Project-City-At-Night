453e611f5ee30cb5e765f5bd50a266959a627e06
Ice Maya <echaymaya@gmail.com>
2018-10-12 15:40:27 +1300

    refactored code using fat arrow function

--- art-project.js
+++ art-project.js
@@ -1,12 +1,14 @@
+document.addEventListener("DOMContentLoaded", cityScape);
 
-var canvas = document.querySelector('canvas');
-var ctx=canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = 300;
-
-
+const  cityScape = () => { 
+    var canvas = document.getElementById('stars');
+    var ctx=canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = 300;
+    multiplyStars();
+}
 //borrowed drawStar function from http://jsfiddle.net/m1erickson/8j6kdf4o/
-function drawStar(cx,cy,spikes,outerRadius,innerRadius, strokeWidth){
+const drawStar = (cx,cy,spikes,outerRadius,innerRadius, strokeWidth) => {
     var rot=Math.PI/2*3;
     var x=cx;
     var y=cy;
@@ -41,7 +43,7 @@ function drawStar(cx,cy,spikes,outerRadius,innerRadius, strokeWidth){
 }
 
 //Create 90 stars with a delay of 5 seconds. Randomise the early stars to appear after 5 seconds and later stars stops appearing after 17 seconds.
-function multiplyStars () {
+const multiplyStars = () => {
     for (var i = 0; i < 90; i++) {
         var delay = Math.random() * 12000 + 5000;
         setTimeout(createStar, delay );    
@@ -50,7 +52,7 @@ function multiplyStars () {
 }
 
 //create a single star and place it in any random location within the canvas height 300px
-function createStar() {
+const createStar = () => {
     var x = Math.random() * window.innerWidth;
     var y = Math.random() * window.innerHeight;
     var outerRadius = (Math.random() * 4) + 3;
@@ -58,10 +60,7 @@ function createStar() {
     drawStar(x, y, 6, outerRadius, innerRadius, 2);
 
 }
-multiplyStars();
 
 //play sound with toggle play and pause feature
-var button = document.getElementById('sound');
-function togglePlay() {
-    return sound.paused ? sound.play() : sound.pause();
-}
+const togglePlay = () => sound.paused ? sound.play() : sound.pause();
+
